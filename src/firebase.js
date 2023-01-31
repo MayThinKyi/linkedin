ba80b6e20ea7bcbea14d4ad712f2d28738b5fea7
MayThinKyi303 <maythinkyi2003@gmail.com>
2023-01-31 18:43:03 +0630

    setting up

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaYyFtNGdUl-XVkoZb_ExGD8-ReBtul_0",
+  authDomain: "react-linkedin-1179a.firebaseapp.com",
+  projectId: "react-linkedin-1179a",
+  storageBucket: "react-linkedin-1179a.appspot.com",
+  messagingSenderId: "164876657938",
+  appId: "1:164876657938:web:d08740e3a529572d7bffcb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
